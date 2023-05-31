7c1afd84e4f8f6d703dc335357b4b2cfdd9d316e
ErAyushi <108145753+ErAyushi@users.noreply.github.com>
2023-05-31 14:30:05 +0530

    Add files via upload
    ##Chatbot API with PostgreSQL Database Integration
    
    This is a Python application that provides a RESTful API for executing queries on a PostgreSQL database. It allows you to fetch data from the public.restaurants table in the database using predefined queries.

---
+++ main3.py
@@ -0,0 +1,47 @@
+import psycopg2
+from flask import Flask, jsonify
+import os
+
+def create_connection():
+    connection = psycopg2.connect(
+        host='192.168.20.97',
+        port='5432',
+        dbname='postgres',
+        user='postgres',
+        password='root'
+    )
+    return connection
+
+app = Flask(__name__)
+
+@app.route('/query1', methods=['GET'])
+def execute_query1():
+    try:
+        connection = create_connection()
+        cursor = connection.cursor()
+        cursor.execute('SELECT "Restaurants" FROM public.restaurants')
+        result = cursor.fetchall()
+        cursor.close()
+        connection.close()
+        return jsonify(result)
+    except Exception as e:
+        return jsonify(error=str(e)), 500
+
+@app.route('/query2', methods=['GET'])
+def execute_query2():
+    try:
+        connection = create_connection()
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM public.restaurants')
+        result = cursor.fetchall()
+        cursor.close()
+        connection.close()
+        return jsonify(result)
+    except Exception as e:
+        return jsonify(error=str(e)), 500
+
+# Add more routes and corresponding query functions as needed
+
+if __name__ == '__main__':
+    app.run(port=5003)
+
